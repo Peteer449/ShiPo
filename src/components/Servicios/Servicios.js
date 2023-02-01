b6a88fbd24f715b320d3ea874934a233fcbaacb6
tghiglione <tomasghiglione@gmail.com>
2023-02-01 19:10:06 -0300

    agregando estilos a servicios

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -6,14 +6,14 @@ import LogoWpp from '../../images/icon-whatsapp.svg';
 export default function Servicios(props){
     return(
         <div className="servicios">  
-            <img src={props.imagen}/>
+            <img src={props.imagen} className='imagenServicio'/>
             <h3 className="tituloServicio">{props.titulo}</h3>
             <p className="descripcionServicio">{props.descripcion}</p>
             <div className="duracionServicio">
                 <img src={LogoDuracion} alt='imagen de reloj' className="logoReloj"/>
-                <p className="duracion"><span>Duración:</span>de 45 a 75 min</p>
+                <p className="duracion"><span>Duración:</span> de 45 a 75 min</p>
             </div>
-            <p>Apartá tu cita por WhatsApp.<br/>¡Te esperamos!</p>
+            <p className="textoServicios">Apartá tu cita por WhatsApp.<br/>¡Te esperamos!</p>
             <div className="reserva">
                 <h4>Quiero una cita</h4>
                 <img src={LogoWpp} alt='logo de whatsapp' className="logoWpp"/>
