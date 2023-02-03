c19459f48dfa87c888a1cb1e81b663069b385f66
tghiglione <tomasghiglione@gmail.com>
2023-02-03 15:35:35 -0300

    barraNav terminada

--- src/components/Navegation/Navegation.js
+++ src/components/Navegation/Navegation.js
@@ -7,28 +7,34 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Logo from '../../images/logo-shipo.svg';
+import { useState } from "react";
 
 export default function Navegation(){
- /*  const categories=[
-    {url:"/", name:"Inicio"},
-    {url:'/list',name:"Personajes"}
-]; */
+  const [menuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const handleClose = () => setMenuOpen(false)
   return(
     <>
-      <Navbar key={'xxl'} expand={'xxl'} className="mb-3 barraNav">
-        <Container fluid>
+      <Navbar key={'false'} expand={'false'} className="mb-3 barraNav">
+        <Container fluid >
           <Link to='/'>
             <Navbar.Brand><img src={Logo} alt='logo de Shipo' className="logo"/></Navbar.Brand>
           </Link>  
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'xxl'}`} variant="light" />
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'false'}`} variant="light" onClick={toggleMenu}/>
           <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${'xxl'}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${'xxl'}`}
+            id={`offcanvasNavbar-expand-${'false'}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${'false'}`}
             placement="end"
             className='barraNavDerecha'
+            restoreFocus={false}
+            show={menuOpen}
+            onHide={handleClose}
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'xxl'}`}>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'false'}`}>
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
@@ -36,41 +42,41 @@ export default function Navegation(){
                 <h3 className="tituloNav">Servicios</h3>
                 <NavDropdown
                   title="Masajes"
-                  id={`offcanvasNavbarDropdown-expand-${'xxl'}`}
+                  id={`offcanvasNavbarDropdown-expand-${'false'}`}
                   className='linkColor'
                   bg='dark'
                 >
                   <NavDropdown.Item >
-                    <NavLink to="/masajes/descontracturantes" className='linkBlanco'>Masajes <br/>descontracturantes</NavLink>
+                    <NavLink onClick={toggleMenu} to="/masajes/descontracturantes" className='linkBlanco'>Masajes <br/>descontracturantes</NavLink>
                   </NavDropdown.Item>
                   <NavDropdown.Item >
-                    <NavLink to='/masajes/piedras_calientes' className='linkBlanco'>Masajes <br/> descontracturantes<br/> + piedras calientes</NavLink>
+                    <NavLink onClick={toggleMenu} to='/masajes/piedras_calientes' className='linkBlanco'>Masajes <br/> descontracturantes<br/> + piedras calientes</NavLink>
                   </NavDropdown.Item>
                   <NavDropdown.Item >
-                    <NavLink to='/masajes/maderoterapia' className='linkBlanco'>Masajes con <br/>maderoterapia</NavLink>
+                    <NavLink onClick={toggleMenu} to='/masajes/maderoterapia' className='linkBlanco'>Masajes con <br/>maderoterapia</NavLink>
                   </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown
                   title="Electrodos"
-                  id={`offcanvasNavbarDropdown-expand-${'xxl'}`}
+                  id={`offcanvasNavbarDropdown-expand-${'false'}`}
                   className='linkColor'
                 >
                   <NavDropdown.Item>
-                    <NavLink to='/electrodos/sesiones' className='linkBlanco'>Sesiones de electrodos</NavLink>
+                    <NavLink onClick={toggleMenu} to='/electrodos/sesiones' className='linkBlanco'>Sesiones de electrodos</NavLink>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <NavLink to='/electrodos/levantamiento_de_gluteos' className='linkBlanco'>Levantamiento de <br/>glúteos + electrodos</NavLink>
+                    <NavLink onClick={toggleMenu} to='/electrodos/levantamiento_de_gluteos' className='linkBlanco'>Levantamiento de <br/>glúteos + electrodos</NavLink>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <NavLink to='/electrodos/reductores' className='linkBlanco'>Masajes reductores + <br/>electrodos</NavLink>
+                    <NavLink onClick={toggleMenu} to='/electrodos/reductores' className='linkBlanco'>Masajes reductores + <br/>electrodos</NavLink>
                   </NavDropdown.Item>
                 </NavDropdown>
-                <NavLink to='/drenaje_linfatico' className='linkColor'>Drenaje Linfático</NavLink>
-                <NavLink to='/belleza_manos_pies' className='linkColor'>Belleza de manos <br/>y pies</NavLink>
-                <NavLink to='/esmalte_semipermanente' className='linkColor'>Esmalte <br/>semipermanente</NavLink>
-                <NavLink to='/depilacion_laser_definitiva' className='linkColor'>Depilación láser <br/>definitiva</NavLink>
+                <NavLink onClick={toggleMenu} to='/drenaje_linfatico' className='linkColor'>Drenaje Linfático</NavLink>
+                <NavLink onClick={toggleMenu} to='/belleza_manos_pies' className='linkColor'>Belleza de manos <br/>y pies</NavLink>
+                <NavLink onClick={toggleMenu} to='/esmalte_semipermanente' className='linkColor'>Esmalte <br/>semipermanente</NavLink>
+                <NavLink onClick={toggleMenu} to='/depilacion_laser_definitiva' className='linkColor'>Depilación láser <br/>definitiva</NavLink>
                 <NavDropdown.Divider/>
-                <NavLink className='linkUbicacion'>Ubicación</NavLink>
+                <NavLink onClick={toggleMenu} className='linkUbicacion'>Ubicación</NavLink>
               </Nav>  
             </Offcanvas.Body>
           </Navbar.Offcanvas>
